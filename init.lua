5483a24743253a9a20b7185b5a645d4ea34220da
aske-wengel-frost <askefrost26@gmail.com>
2026-05-28 11:49:39 +0200

    add: new lsp for json

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.o.tabstop = 8
 vim.o.signcolumn = "yes"
 vim.o.winborder = "rounded"
 
-local servers = { "clangd", "lua_ls", "tinymist", "pyright", "ruby_lsp", "ts_ls" }
+local servers = { "clangd", "lua_ls", "tinymist", "pyright", "ruby_lsp", "ts_ls", "jsonls" }
 vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/nvim-mini/mini.nvim" },
@@ -20,7 +20,7 @@ vim.pack.add({
 	{ src = "https://github.com/akinsho/toggleterm.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/folke/trouble.nvim" },
-	{ src = "https://github.com/oskarnurm/koda.nvim"}
+	{ src = "https://github.com/oskarnurm/koda.nvim" }
 })
 
 
